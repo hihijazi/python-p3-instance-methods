685611e4669cdadb0309fa131502b89d7de9cc6b
hadil <hihijazi@gmail.com>
2024-01-11 17:27:22 -0500

    save changes

--- lib/dog.py
+++ lib/dog.py
@@ -4,4 +4,13 @@ class Dog:
     # Class body goes here
 
     #Instance method definition
+    def bark(self): 
+        print("Woof!")
+
+    def sit(self):  
+        print("The dog is sitting.")
     pass
+
+bingo = Dog()   
+bingo.bark()
+bingo.sit() 
